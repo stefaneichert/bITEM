d4f9f716b8c218a205a34dadf5ca386e204bd310
stefaneichert <stefan.eichert@univie.ac.at>
2023-10-30 10:38:02 +0100

    random 3d model from files for index

--- bitem/views/index.py
+++ bitem/views/index.py
@@ -1,14 +1,39 @@
 from pathlib import Path
 
-from flask import render_template
+from flask import render_template, g
 from bitem import app
-import os, random
+import random
 
 @app.route('/')
 def index():
     # get random 3d model
-    path = Path(app.root_path) / 'static' / '3dmodels'
-    file = random.choice(os.listdir(path))
-    #file = random.choice(os.listdir("tests/bitem/static/3dmodels"))
+    classes = app.config['VIEW_CLASSES']
+    root = str(app.config['CASE_STUDY'])
+    all_classes = (classes['actors'] + classes['items'] + classes['places'] +
+                   classes['events'])
+    all_classes = str(list(all_classes))
 
-    return render_template("/index/index.html", model=file)
+    sql = """
+        SELECT * FROM (SELECT id,
+            MAX(CASE WHEN mimetype = '3d' THEN filename END) AS model,
+            MAX(CASE WHEN mimetype = 'poster' THEN filename END) AS poster
+                FROM (SELECT l.range_id AS id, f.filename, f.mimetype
+                    FROM bitem.files f
+                       JOIN model.link l ON f.id = l.domain_id
+                       JOIN model.entity e ON e.id = l.range_id
+                       WHERE l.property_code = 'P67'
+                       AND f.mimetype IN ('3d', 'poster', 'img')
+                       AND l.range_id IN (SELECT ids
+                               FROM bitem.get_entities(
+                           ARRAY """ + all_classes + """ , """ + root + """
+                       ))) a GROUP BY a.id) b WHERE poster IS NOT NULL
+    """
+
+    g.cursor.execute(sql)
+    result = g.cursor.fetchall()
+
+    random_record = random.choice(result)
+    model = app.config['OPENATLAS_UPLOAD_FOLDER'] + '/' + random_record.model
+    poster = app.config['OPENATLAS_UPLOAD_FOLDER'] + '/' + random_record.poster
+    
+    return render_template("/index/index.html", model=model, poster=poster)
